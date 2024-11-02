c957b522b54b3e7d958756c9067e49b897db2ab9
foxiyofox <foxiyofox@gmail.com>
2024-11-02 14:18:33 -0300

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,5 +5,7 @@ export PLATFORM_VERSION=13
 export ANDROID_MAJOR_VERSION=t
 export ARCH=arm64
 
+# make -mrproper && clean= it's not working just clean the output folder, if the source is not dirty, if it is dirty download it again
+
 make -j8 exynos850-a12snsxx_defconfig O=out ARCH=arm64
 make -j8 O=out ARCH=arm64
